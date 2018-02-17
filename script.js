0039764238cf3fa6ff770c7d2d257f4a38485661
Lou Stringer <louise.stringer@gmail.com>
2018-02-17 15:32:01 +0000

    add event listeners for keypress

--- script.js
+++ script.js
@@ -11,36 +11,61 @@ var text = document.querySelector("p.text");
 
 // generate random hexcode colour
 function randomColor() {
-  return "#" + Math.random().toString(16).slice(2,8);
-}
-
-// generate colours, apply to boxes and add relevant classes & event listeners
-function setColors() {
-  colorList = [];
-  targetColor = randomColor();
-  text.textContent = targetColor;
-  targetPosition = Math.floor(Math.random()*(numOfBoxes));
-  for (i = 0; i < numOfBoxes; i++) {
-    if (i!=targetPosition) {
-      colorList[i] = randomColor();
-      colorBoxes[i].addEventListener("click", fillerClicked);
-    } else {
-      colorList[i] = targetColor;
-      colorBoxes[i].addEventListener("click", targetClicked);
-    };
-    colorBoxes[i].style.background = colorList[i];
-    colorBoxes[i].style.border = "2px solid colorList[i]";
+  for (var i = 0; i < numOfBoxes; i++) {
+    colorList[i] = "#" + Math.random().toString(16).slice(2,8);
   };
 }
 
-// colour clicks
+// generate colours, apply to boxes and add relevant
+// classes, attributes & event listeners
+function setColors() {
+  randomColor();
+  targetPosition = Math.floor(Math.random()*(numOfBoxes));
+  targetColor = colorList[targetPosition];
+  text.textContent = targetColor;
+  for (var i = 0; i < numOfBoxes; i++) {
+    if (i!=targetPosition) {
+      colorBoxes[i].addEventListener("click", fillerClicked);
+      colorBoxes[i].addEventListener("keypress", fillerTabbed);
+    } else {
+      colorBoxes[i].addEventListener("click", targetClicked);
+      colorBoxes[i].addEventListener("keypress", targetTabbed);
+    };
+    colorBoxes[i].style.background = colorList[i];
+    colorBoxes[i].style.opacity = 1;
+    colorBoxes[i].classList.add("notClicked");
+    colorBoxes[i].setAttribute("tabindex", i+1);
+    };
+}
+
+// colour click/tab functions
 function targetClicked() {
   for (var i = 0; i < numOfBoxes; i++) {
     text.textContent = "yay, that's " + targetColor;
     colorBoxes[i].style.background = targetColor;
-    colorBoxes[i].style.border = "0px";
+    colorBoxes[i].style.opacity = 1;
+    colorBoxes[i].classList.remove("notClicked");
+    colorBoxes[i].setAttribute("tabindex", "-1");
     colorBoxes[i].removeEventListener("click", targetClicked);
     colorBoxes[i].removeEventListener("click", fillerClicked);
+    colorBoxes[i].removeEventListener("keypress", targetTabbed);
+    colorBoxes[i].removeEventListener("keypress", fillerTabbed);
+  };
+}
+
+function targetTabbed(event) {
+  if (event.keyCode === 13) {
+    for (var i = 0; i < numOfBoxes; i++) {
+      text.textContent = "yay, that's " + targetColor;
+      colorBoxes[i].style.background = targetColor;
+      colorBoxes[i].style.opacity = 1;
+      colorBoxes[i].classList.remove("notClicked");
+      colorBoxes[i].setAttribute("tabindex", "-1");
+      colorBoxes[i].removeEventListener("click", targetClicked);
+      colorBoxes[i].removeEventListener("click", fillerClicked);
+      colorBoxes[i].removeEventListener("keypress", targetTabbed);
+      colorBoxes[i].removeEventListener("keypress", fillerTabbed);
+    };
   };
 }
 
@@ -48,8 +73,22 @@ function fillerClicked() {
   for (var i = 0; i < numOfBoxes; i++) {
     if (colorBoxes[i] === this) {
       text.textContent = targetColor + "  (that was " + colorList[i] + ")";
-      colorBoxes[i].style.background = "white";
-      colorBoxes[i].style.border = "1px solid" + colorList[i];
+      colorBoxes[i].style.opacity = 0.15;
+      colorBoxes[i].classList.remove("notClicked");
+      colorBoxes[i].setAttribute("tabindex", "-1");
+    };
+  };
+}
+
+function fillerTabbed(event) {
+  if (event.keyCode === 13) {
+    for (var i = 0; i < numOfBoxes; i++) {
+      if (colorBoxes[i] === this) {
+        text.textContent = targetColor + "  (that was " + colorList[i] + ")";
+        colorBoxes[i].style.opacity = 0.15;
+        colorBoxes[i].classList.remove("notClicked");
+        colorBoxes[i].setAttribute("tabindex", "-1");
+      };
     };
   };
 }
