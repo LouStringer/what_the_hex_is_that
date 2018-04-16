a11022eda421b0925057684c13a451d2fb59bf98
Lou Stringer <louise.stringer@gmail.com>
2018-04-16 19:08:57 +0100

    refactor js: DRY up code and use module

--- script.js
+++ script.js
@@ -1,128 +1,121 @@
 // What the hex is that?
 // A hex code guessing game
 
-// create variables/select objects required
-var targetColor = "";
-var targetPosition = "";
-var numOfBoxes = 6; // for default medium level
-var colorList = [];
-var colorBoxes = document.querySelectorAll(".color");
-var text = document.querySelector("h2.text");
+const hexGame = (() => {
+  // create variables/select objects required
+  var targetColor = "";
+  var targetPosition = "";
+  var numOfBoxes = 6; // for default medium level
+  var colorList = [];
 
-// generate random hexcode colour
-function randomColor() {
-  for (var i = 0; i < numOfBoxes; i++) {
-    colorList[i] = "#" + Math.random().toString(16).slice(2,8);
-  };
-}
+  // Grab DOM objects
+  let main = document.querySelector("main");
+  let text = document.querySelector("h2.text");
+  let colorBoxes = Array.from(document.querySelectorAll(".color"));
+  let levelButtons = document.querySelectorAll("button.level");
+  let resetButton = document.querySelector(".reset");
+  let instructionsDiv = document.querySelector("div.instructions");
+  let instructionsButton = document.querySelector("button.instructions");
 
-// generate colours, apply to boxes and add relevant
-// classes, attributes & event listeners
-function setColors() {
-  randomColor();
-  targetPosition = Math.floor(Math.random()*(numOfBoxes));
-  targetColor = colorList[targetPosition];
-  text.textContent = targetColor;
-  colorBoxes[targetPosition].classList.add("target");
-  for (var i = 0; i < numOfBoxes; i++) {
-    if (i !== targetPosition) {
-      colorBoxes[i].addEventListener("click", fillerClicked);
-      colorBoxes[i].addEventListener("keypress", fillerTabbed);
-    } else {
-      colorBoxes[i].addEventListener("click", targetClicked);
-      colorBoxes[i].addEventListener("keypress", targetTabbed);
-    };
-    colorBoxes[i].style.background = colorList[i];
-    colorBoxes[i].style.opacity = 1;
-    colorBoxes[i].classList.add("notClicked");
-    colorBoxes[i].setAttribute("tabindex", i+1);
-    };
-}
-
-// colour click/tab functions
-function targetClicked() {
-  for (var i = 0; i < numOfBoxes; i++) {
-    text.textContent = "yay, that's " + targetColor;
-    colorBoxes[i].style.background = targetColor;
-    colorBoxes[i].style.opacity = 1;
-    colorBoxes[i].classList.remove("notClicked");
-    colorBoxes[i].setAttribute("tabindex", "-1");
-    colorBoxes[i].removeEventListener("click", targetClicked);
-    colorBoxes[i].removeEventListener("click", fillerClicked);
-    colorBoxes[i].removeEventListener("keypress", targetTabbed);
-    colorBoxes[i].removeEventListener("keypress", fillerTabbed);
-  };
-}
-
-function targetTabbed(event) {
-  if (event.keyCode === 13) {
+  // generate random hexcode colour
+  function randomColor() {
     for (var i = 0; i < numOfBoxes; i++) {
-      text.textContent = "yay, that's " + targetColor;
-      colorBoxes[i].style.background = targetColor;
-      colorBoxes[i].style.opacity = 1;
-      colorBoxes[i].classList.remove("notClicked");
-      colorBoxes[i].setAttribute("tabindex", "-1");
-      colorBoxes[i].removeEventListener("click", targetClicked);
-      colorBoxes[i].removeEventListener("click", fillerClicked);
-      colorBoxes[i].removeEventListener("keypress", targetTabbed);
-      colorBoxes[i].removeEventListener("keypress", fillerTabbed);
+      colorList[i] = "#" + Math.random().toString(16).slice(2,8);
     };
-  };
-}
+  }
 
-function fillerClicked() {
-  for (var i = 0; i < numOfBoxes; i++) {
-    if (colorBoxes[i] === this) {
+  // generate colours, apply to boxes and add relevant
+  // classes, attributes & event listeners
+  function setTarget() {
+    targetPosition = Math.floor(Math.random()*(numOfBoxes));
+    targetColor = colorList[targetPosition];
+    text.textContent = targetColor;
+    colorBoxes[targetPosition].classList.add("target");
+  }
+
+  function prepBoxes() {
+    for (var i = 0; i < numOfBoxes; i++) {
+      colorBoxes[i].addEventListener("click", colorClicked);
+      colorBoxes[i].addEventListener("keypress", colorTabbed);
+      colorBoxes[i].style.background = colorList[i];
+      colorBoxes[i].style.opacity = 1;
+      colorBoxes[i].classList.add("notClicked");
+      colorBoxes[i].setAttribute("tabindex", i+1);
+      };
+  }
+
+  function setColors() {
+    randomColor();
+    setTarget();
+    prepBoxes();
+  }
+
+  // colour click/tab functions
+  function removeEventLists(box) {
+    box.removeEventListener("click", colorClicked);
+    box.removeEventListener("keypress", colorTabbed);
+    box.setAttribute("tabindex", "-1");
+    box.classList.remove("notClicked", "target");
+  }
+
+  function colorClicked() {
+    const i = colorBoxes.indexOf(this)
+    if (i === targetPosition) {
+      text.textContent = "yay, that's " + targetColor;
+      for (box of colorBoxes) {
+        box.style.background = targetColor;
+        box.style.opacity = 1;
+        removeEventLists(box);
+      };
+    } else {
       text.textContent = targetColor + "  (that was " + colorList[i] + ")";
       colorBoxes[i].style.opacity = 0.15;
-      colorBoxes[i].classList.remove("notClicked");
-      colorBoxes[i].setAttribute("tabindex", "-1");
+      removeEventLists(colorBoxes[i]);
     };
-  };
-}
+  }
 
-function fillerTabbed(event) {
-  if (event.keyCode === 13) {
-    for (var i = 0; i < numOfBoxes; i++) {
-      if (colorBoxes[i] === this) {
-        text.textContent = targetColor + "  (that was " + colorList[i] + ")";
-        colorBoxes[i].style.opacity = 0.15;
-        colorBoxes[i].classList.remove("notClicked");
-        colorBoxes[i].setAttribute("tabindex", "-1");
-      };
+  function colorTabbed(event) {
+    if (event.keyCode === 13) {
+      colorClicked.call(this);
     };
-  };
-}
+  }
 
-// change levels
-function changeLevel() {
-  colorBoxes = document.querySelectorAll(".color");
-  if (this.textContent === "easy") {
-    numOfBoxes = 3;
-  } else if (this.textContent === "medium") {
-    numOfBoxes = 6;
-  } else {
-    numOfBoxes = 9;
-  };
-  for (var i = 0; i < colorBoxes.length; i++) {
-    i < numOfBoxes ? colorBoxes[i].classList.remove("hide") : colorBoxes[i].classList.add("hide");
-  };
-  setColors();
-}
-var levelButtons = document.querySelectorAll("button.level");
-for (var i = 0; i < levelButtons.length; i++) {
-  levelButtons[i].addEventListener("click", changeLevel);
-};
+  // change levels
+  function changeLevel() {
+    if (this.textContent === "easy") {
+      numOfBoxes = 3;
+    } else if (this.textContent === "medium") {
+      numOfBoxes = 6;
+    } else {
+      numOfBoxes = 9;
+    };
+    for (var i = 0; i < colorBoxes.length; i++) {
+      i < numOfBoxes ? colorBoxes[i].classList.remove("hide") : colorBoxes[i].classList.add("hide");
+    };
+    setColors();
+  }
 
-// New colours reset button
-document.querySelector(".reset").addEventListener("click", setColors);
+  for (var i = 0; i < levelButtons.length; i++) {
+    levelButtons[i].addEventListener("click", changeLevel);
+  };
 
-// show/hide instructions
-document.querySelector("button.instructions").addEventListener("click", function () {
-  document.querySelector("div.instructions").classList.toggle("hide");
-  document.querySelector("main").classList.toggle("hide");
-  setColors();
-});
+  // new colours reset button
+  resetButton.addEventListener("click", setColors);
+
+  // show/hide instructions
+  function instructions() {
+    instructionsDiv.classList.toggle("hide");
+    main.classList.toggle("hide");
+    setColors();
+  }
+  instructionsButton.addEventListener("click", instructions)
+
+  // return function to initialise game
+  return {
+    setColors
+  }
+})();
+
 
 // Run the game!
-setColors();
+hexGame.setColors();
